fe6e285a674a94926da4716f4b5f5a064385ead2
Monsieur <jalalmoo13822@gmail.com>
2024-06-30 03:27:43 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
                 'resources/js/meating_room.js',
                 'resources/js/auth/register.js',
                 'resources/js/auth/profile.js',
-                'resources/js/auth/login.js',
                 'resources/js/auth/ban_mod_list.js',
                 'resources/js/auth/phone_verification.js',
                 'resources/js/python/voice_to_speach_form.js',
